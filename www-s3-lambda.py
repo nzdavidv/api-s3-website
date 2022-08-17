641053f0abf61d6187f74d6cedd96f8619650e2e
nzdavidv <79499681+nzdavidv@users.noreply.github.com>
2022-08-17 18:06:38 +1200

    Update www-s3-lambda.py

--- www-s3-lambda.py
+++ www-s3-lambda.py
@@ -1,25 +1,33 @@
 import json
 import os
+import base64
+import hashlib
+import hmac
+import logging
 import boto3
 from io import BytesIO
 
 s3 = boto3.resource('s3')
-bucket = s3.Bucket('392495242865-s3-my-website')
+
+bucketname = os.environ['BUCKETNAME']
+
+bucket = s3.Bucket(bucketname)
 
 html=''
 
 def readhtmlfile(filename):
-    filehand = s3.Object('392495242865-s3-my-website',filename)
+    filehand = s3.Object(bucketname,filename)
     body = filehand.get()['Body'].read()
     html=body.decode('utf8')
     return html
     
 def readbinfile(filename):
-    filehand = s3.Object('392495242865-s3-my-website',filename)
+    filehand = s3.Object(bucketname,filename)
     file_byte_string = filehand.get()['Body'].read()
     return Image.open(BytesIO(file_byte_string))
 
 def lambda_handler(event, context):
+    
     filereq = event['rawPath']
     filereqn=filereq[1:]
     if ('.htm' in filereq) or ('.js' in filereq):
