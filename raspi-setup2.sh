d4f2b3f576624acb299795f253d1d3bf3b1a81a1
nzdavidv <79499681+nzdavidv@users.noreply.github.com>
2022-08-27 18:39:45 +1200

    Update raspi-setup2.sh

--- raspi-setup2.sh
+++ raspi-setup2.sh
@@ -2,15 +2,4 @@ systemctl enable mysql.service ;  systemctl start mysql.service
 service cron start 
 systemctl enable cron
 chown www /var/www/html
-
-echo "enter mysql root password"; read mpasswd
-mysql_secure_installation <<EOF
-
-y
-${mpasswd}
-${mpasswd}
-y
-y
-y
-y
-EOF
+mysql_secure_installation 
