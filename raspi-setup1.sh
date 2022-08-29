78cf8014b73ff365324207326ca916024addf6f3
nzdavidv <79499681+nzdavidv@users.noreply.github.com>
2022-08-29 17:37:54 +1200

    Update raspi-setup1.sh

--- raspi-setup1.sh
+++ raspi-setup1.sh
@@ -5,10 +5,14 @@ echo "setup packages"
 apt update
 apt upgrade
 apt install apache2 php mysql-server php-mysql tcptraceroute bc expect dos2unix telnet php-cli libapache2-mod-php postfix mailutils gh
-#for postfix setup 
+
+systemctl enable mysql.service ;  systemctl start mysql.service
+service cron start 
+systemctl enable cron
 
 echo "setup www user"
 useradd -m -d  /home/www -s /bin/bash www
 mkdir ~www/bin
 chown www:www ~www/bin
 echo umask 022 >> ~www/.profile
+chown www /var/www/html
